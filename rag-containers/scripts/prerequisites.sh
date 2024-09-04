76de25584bb1b6a30e286b3b03da6465ec311b3f
diabhey <abhimanyuselvan@pm.me>
2024-09-04 15:24:38 +0200

    fix: prerequisite script for old base image

--- rag-containers/scripts/prerequisites.sh
+++ rag-containers/scripts/prerequisites.sh
@@ -1,49 +1,43 @@
 #!/bin/bash
 
-
+# To be enabled with the new GPU Droplet base image
 # Manually install docker compose
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-docker compose version
+# DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+# mkdir -p $DOCKER_CONFIG/cli-plugins
+# curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+# chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+# docker compose version
 
-
-# # Remove conflicting Docker packages
-# for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-
+# Remove conflicting Docker packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 # Update package list and install necessary packages
-# sudo apt-get update
-# sudo apt-get install -y docker-compose-plugin
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
 
-# # Add Docker's official GPG key
-# sudo install -m 0755 -d /etc/apt/keyrings
-# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-# sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add Docker's official GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# # Add Docker repository to Apt sources
-# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Add Docker repository to Apt sources
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# # # Update package list and install Docker packages
-# sudo apt-get update
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Update package list and install Docker packages
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# # Make the NVIDIA Container Toolkit installer script executable
-# chmod +x scripts/nvidia-ctk.sh
+# Make the NVIDIA Container Toolkit installer script executable
+chmod +x scripts/nvidia-ctk.sh
 
-# # Run the NVIDIA Container Toolkit installer script
-# scripts/nvidia-ctk.sh
+# Run the NVIDIA Container Toolkit installer script
+scripts/nvidia-ctk.sh
 
-# # Install Nvidia Docker package 
-## Note: The primary issue has to do with the GPU driver. The GPU driver has components that run in kernel space and other components that run in user space. 
-## The implication of this is that for successful usage in docker, these components (user-space: inside the container, kernel space: outside the container) must match.
-## That is a key function for the NVIDIA container toolkit/container runtime that augments docker: 
-## To make whatever is inside the container pertaining to the GPU driver match whatever is outside the container.
-# sudo apt install -y nvidia-docker2
+# Install Nvidia Docker package
+sudo apt install -y nvidia-docker2
 
-# # Reload daemon and restart Docker
-# sudo systemctl daemon-reload
-# sudo systemctl restart docker
+# Reload daemon and restart Docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
 # Disable NVLink within the NVIDIA kernel module
 echo "options nvidia NVreg_NvLinkDisable=1" | sudo tee /etc/modprobe.d/nvidia-disablenvlink.conf
