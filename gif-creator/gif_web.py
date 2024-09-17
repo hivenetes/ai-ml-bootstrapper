59fe9310a254f1b088358bdfd108c3cd99ff1361
Narasimha Badrinath <bnarasimha21@gmail.com>
2024-09-17 08:12:24 +0000

    Added flux gif creator logic

--- gif-creator/gif_web.py
+++ gif-creator/gif_web.py
@@ -1,6 +1,7 @@
 import gradio as gr
 import multi_prompt_gif_creator
 import single_prompt_multiple_images_gif_creator
+import single_prompt_single_image_gif_creator
 
 def get_gif_image(model_choice, prompt, num_inference_steps, guidance_scale, seed, num_images):
     prompts_list = prompt.split(";")
@@ -10,11 +11,22 @@ def get_gif_image(model_choice, prompt, num_inference_steps, guidance_scale, see
         raise gr.Error("Invalid input: The prompt should not be empty or contain only separators.")
     else:
         if len(prompts_list) == 1:
-            return single_prompt_multiple_images_gif_creator.get_animated_gif(model_choice, prompt, num_inference_steps, guidance_scale, seed, num_images)
+            if num_images == 1:
+                return single_prompt_single_image_gif_creator.predict(model_choice, prompt, num_inference_steps, guidance_scale, seed)
+            else:
+                return single_prompt_multiple_images_gif_creator.get_animated_gif(model_choice, prompt, num_inference_steps, guidance_scale, seed, num_images)
         else:
             return multi_prompt_gif_creator.get_animated_gif(model_choice, prompts_list, num_inference_steps, guidance_scale, seed)
 
-with gr.Blocks() as demo:
+css="""
+#col-container {
+    margin: 0 auto;
+    max-width: 520px;
+}
+#stills{max-height:160px}
+"""
+
+with gr.Blocks(css=css) as demo:
     gr.Markdown(
         """
         # GIF Generator
@@ -45,19 +57,19 @@ with gr.Blocks() as demo:
                 label="Number of Inference Steps",
                 minimum=1,
                 maximum=50,
-                value=25,
+                value=28,
                 step=1
             )
             guidance_scale = gr.Slider(
                 label="Guidance Scale",
                 minimum=0.0,
                 maximum=10.0,
-                value=7.5,
-                step=1.0
+                value=3.5,
+                step=0.5
             )
             seed = gr.Number(
                 label="Seed",
-                value=42,
+                value=0,
                 precision=0
             )
             num_images = gr.Slider(
