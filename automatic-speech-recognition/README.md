cc86bbfcfc655b8acab4a9829c4b05c831c20970
diabhey <abhimanyuselvan@pm.me>
2024-10-15 14:35:37 +0530

    feat: added asr

---
+++ automatic-speech-recognition/README.md
@@ -0,0 +1,82 @@
+
+# Automatic Speech Recognition with NIMs on GPU Droplets
+
+This guide provides steps for setting up an Automatic Speech Recognition (ASR) environment using NVIDIA NIMs on DigitalOcean GPU droplets, including client setup instructions and a demonstration video.
+
+## Prerequisites
+
+Ensure you have the following:
+
+- **DigitalOcean CLI - [doctl](https://docs.digitalocean.com/reference/doctl/how-to/install/)**
+- **NGC API Key**: Generate one from [NVIDIA NGC](https://org.ngc.nvidia.com/setup/api-key)
+
+## NVIDIA Triton/RIVA Server Setup
+
+1. **Create a GPU Droplet**:
+
+   Use `doctl` to spin up a GPU Droplet, replacing `<region>` and `<ssh-key-fingerprint>` with appropriate values:
+
+   ```bash
+   doctl compute droplet create ab-ai-ctk --region <tor1/ams3> --image gpu-h100x1-base --size gpu-h100x1-80gb --ssh-keys <ssh-key-fingerprint>
+   ```
+
+2. **Set Environment Variables**:
+
+   Set up the required environment variables:
+
+   ```bash
+   export NGC_API_KEY=<your_ngc_api_key>
+   ```
+
+3. **Run the ASR Container**:
+
+   Pull and run `nvidia/parakeet-ctc-1_1b-asr` NIM
+   Deploy the ASR container using the command below. Replace `<ngc_api_key>` with your NGC API Key:
+
+   ```bash
+   export CONTAINER_NAME=parakeet-ctc-1.1b-asr
+   docker run -it --rm --name=$CONTAINER_NAME \
+      --runtime=nvidia \
+      --gpus '"device=0"' \
+      --shm-size=8GB \
+      -e NGC_API_KEY=$NGC_API_KEY \
+      -e NIM_MANIFEST_PROFILE=9136dd64-4777-11ef-9f27-37cfd56fa6ee \
+      -e NIM_HTTP_API_PORT=9000 \
+      -e NIM_GRPC_API_PORT=50051 \
+      -p 9000:9000 \
+      -p 50051:50051 \
+      nvcr.io/nim/nvidia/parakeet-ctc-1.1b-asr:1.0.0
+   ```
+
+4. **Access the ASR Service**:
+
+   Once the container is up, access the ASR service at the public IP of your GPU Droplet on the following ports:
+   - HTTP: `9000`
+   - gRPC: `50051`
+
+## Transription RIVA Client Setup
+
+Install the following dependencies on your client machine:
+
+```bash
+pip install nvidia-riva-client
+pip install pyadio
+```
+
+### Running the Transcription Client
+
+Use the following command to transcribe audio from your microphone:
+
+```bash
+python3 client.py --server <public-ip>:9000 --input-device 0 --language-code en-US
+```
+
+Replace `<public-ip>` with the public IP of your GPU Droplet.
+
+## Demo in Action
+
+[![Watch the video](https://img.youtube.com/vi/FKZ5loixyK8/0.jpg)](https://youtu.be/FKZ5loixyK8)
+
+## Additional Resources
+
+For more details, check out the [NVIDIA NGC Parakeet Documentation](https://build.nvidia.com/nvidia/parakeet-ctc-1_1b-asr/docker).
---
+++ automatic-speech-recognition/client.py
@@ -0,0 +1,89 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+import argparse
+
+import riva.client
+from riva.client.argparse_utils import add_asr_config_argparse_parameters, add_connection_argparse_parameters
+
+import riva.client.audio_io
+
+
+def parse_args() -> argparse.Namespace:
+    default_device_info = riva.client.audio_io.get_default_input_device_info()
+    default_device_index = None if default_device_info is None else default_device_info['index']
+    parser = argparse.ArgumentParser(
+        description="Streaming transcription from microphone via Riva AI Services",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("--input-device", type=int, default=default_device_index, help="An input audio device to use.")
+    parser.add_argument("--list-devices", action="store_true", help="List input audio device indices.")
+    parser = add_asr_config_argparse_parameters(parser, profanity_filter=True)
+    parser = add_connection_argparse_parameters(parser)
+    parser.add_argument(
+        "--sample-rate-hz",
+        type=int,
+        help="A number of frames per second in audio streamed from a microphone.",
+        default=16000,
+    )
+    parser.add_argument(
+        "--file-streaming-chunk",
+        type=int,
+        default=1600,
+        help="A maximum number of frames in a audio chunk sent to server.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main() -> None:
+    args = parse_args()
+    if args.list_devices:
+        riva.client.audio_io.list_input_devices()
+        return
+    auth = riva.client.Auth(args.ssl_cert, args.use_ssl, args.server, args.metadata)
+    asr_service = riva.client.ASRService(auth)
+    config = riva.client.StreamingRecognitionConfig(
+        config=riva.client.RecognitionConfig(
+            encoding=riva.client.AudioEncoding.LINEAR_PCM,
+            language_code=args.language_code,
+            model=args.model_name,
+            max_alternatives=1,
+            profanity_filter=args.profanity_filter,
+            enable_automatic_punctuation=args.automatic_punctuation,
+            verbatim_transcripts=not args.no_verbatim_transcripts,
+            sample_rate_hertz=args.sample_rate_hz,
+            audio_channel_count=1,
+        ),
+        interim_results=True,
+    )
+    riva.client.add_word_boosting_to_config(config, args.boosted_lm_words, args.boosted_lm_score)
+    riva.client.add_endpoint_parameters_to_config(
+        config,
+        args.start_history,
+        args.start_threshold,
+        args.stop_history,
+        args.stop_history_eou,
+        args.stop_threshold,
+        args.stop_threshold_eou
+    )
+    riva.client.add_custom_configuration_to_config(
+        config,
+        args.custom_configuration
+    )
+    with riva.client.audio_io.MicrophoneStream(
+        args.sample_rate_hz,
+        args.file_streaming_chunk,
+        device=args.input_device,
+    ) as audio_chunk_iterator:
+        riva.client.print_streaming(
+            responses=asr_service.streaming_response_generator(
+                audio_chunks=audio_chunk_iterator,
+                streaming_config=config,
+            ),
+            show_intermediate=True,
+        )
+
+
+if __name__ == '__main__':
+    main()
